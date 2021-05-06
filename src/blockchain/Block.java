3c950eadd5e7042a3ab996937eb0eb7ce959fbd5
Tatsu Caleum <shinyakogami2010@gmail.com>
2021-05-06 23:13:35 +0700

    add Block

--- src/blockchain/Block.java
+++ src/blockchain/Block.java
@@ -1,16 +1,22 @@
-package blockchain;
+
 import java.util.*;
 public class Block 
 {
-    private String hash;
-    private String previousHash;
-    private String data;
-    private long timeStamp;
-    public Block(String hash, String previousHash, String data)
+    public String hash;
+    public String previousHash;
+    public String data;
+    public long timeStamp;
+    public Block(String data, String previousHash)
     {
-        this.timeStamp = new Date().
+        this.previousHash = previousHash;
+        this.timeStamp = new Date().getTime();
+        this.data = data;
+        this.hash = caculateHash();
     }
-    public static void main(String[] args) {
-        
+    public String caculateHash()
+    {
+        //caculate hash
+        return Utis.useHash(previousHash + Long.toString(timeStamp) + data);
     }
+
 }
