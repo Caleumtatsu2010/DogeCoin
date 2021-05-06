62591104b13992431d9e1e1868c4c716c069377e
Tatsu Caleum <shinyakogami2010@gmail.com>
2021-05-06 22:08:55 +0700

    Initial commit

---
+++ src/blockchain/Block.java
@@ -0,0 +1,16 @@
+package blockchain;
+import java.util.*;
+public class Block 
+{
+    private String hash;
+    private String previousHash;
+    private String data;
+    private long timeStamp;
+    public Block(String hash, String previousHash, String data)
+    {
+        this.timeStamp = new Date().
+    }
+    public static void main(String[] args) {
+        
+    }
+}
